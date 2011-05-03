64fdf85feb38c46c3b1acc9100248ebbd5fcc575
Pierre Gasté <pierregaste@gmail.com>
2011-05-03 15:00:43 +0200

    Commit pour rétablir le bon repo

--- app/controllers/Car.java
+++ app/controllers/Car.java
@@ -17,9 +17,41 @@ public class Car extends Controller {
     }
     
     /**
-     * Listing des voitures de l'utilisateur
+     * Liste des voitures du compte connecté
      */
     public static void list()
+    {
+        render();
+    }
+    
+    /**
+     * Ajout une voiture pour le compte connecté
+     */
+    public static void add()
+    {
+        render();
+    }
+    
+    /**
+     * Edit une voiture pour le compte connecté
+     */
+    public static void edit()
+    {
+        render();
+    }
+    
+    /**
+     * Supprime une voiture pour le compte connecté
+     */
+    public static void delete()
+    {
+        
+    }
+    
+    /**
+     * Affiche les informations d'une voiture
+     */
+    public static void details()
     {
         render();
     }
