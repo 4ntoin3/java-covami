18d5e87eb23e5af31dea2f1ec7e0393b1fdafa19
Antoine <Antoine@192.168.1.10>
2011-05-04 01:08:36 +0200

    Ajout des methodes pour les POST

--- app/controllers/Car.java
+++ app/controllers/Car.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.util.List;
 import play.mvc.Controller;
 
 /**
@@ -21,15 +22,22 @@ public class Car extends Controller {
      */
     public static void list()
     {
-        render();
+        List<Car> cars =  models.Car.find("byOwner", User.connected()).fetch();
+        render(cars);
     }
     
     /**
      * Ajout une voiture pour le compte connecté
      */
     public static void add()
-    {
+    {        
         render();
+    }
+    
+    public static void add(String name, int cost){               
+        new models.Car(name, cost, User.connected()).save();
+        
+        redirect("/car/list");
     }
     
     /**
@@ -40,20 +48,33 @@ public class Car extends Controller {
         render();
     }
     
+    public static void edit(int id, String name, int cost){
+        models.Car car = models.Car.findById(id);
+        car.name = name;
+        car.cost = cost;
+        car.save();
+        
+        redirect("/car/list");
+    }
+    
     /**
      * Supprime une voiture pour le compte connecté
      */
-    public static void delete()
+    public static void delete(int id)
     {
+        models.Car car = models.Car.findById(id);
+        car.delete();
         
+        redirect("/car/list");
     }
     
     /**
      * Affiche les informations d'une voiture
      */
-    public static void details()
+    public static void details(int id)
     {
-        render();
+        models.Car car = models.Car.findById(id);
+        render(car);
     }
     
 }
