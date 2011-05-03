552cd6b09779883a980b7f4fa8687e0631864e09
Antoine <Antoine@192.168.1.10>
2011-05-03 22:10:55 +0200

    ajout des attributs et du constructeur

--- app/models/WayParticipation.java
+++ app/models/WayParticipation.java
@@ -4,7 +4,13 @@
  */
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import org.joda.time.DateTime;
+import play.data.validation.Required;
 import play.db.jpa.Model;
 
 /**
@@ -14,4 +20,23 @@ import play.db.jpa.Model;
 @Entity
 public class WayParticipation extends Model{
     
+    @ManyToOne
+    @Required
+    public Way way;
+    
+    @OneToMany
+    @Required
+    public List<User> participants;
+    
+    @Required
+    public DateTime dateHourRequest;
+    
+    public DateTime dateHourResponse;
+
+    public WayParticipation(Way way, List<User> participants) {
+        this.way = way;
+        this.participants = new ArrayList<User>();
+        this.participants = participants;
+        this.dateHourRequest = new DateTime();
+    }   
 }
