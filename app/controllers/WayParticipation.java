d37deeef89a4644cba8c2ce80b8e6a64c3d69ef2
Antoine <Antoine@192.168.1.10>
2011-05-03 22:09:46 +0200

    ajout des méthodes add, edit, cancel, accept, refuse

--- app/controllers/WayParticipation.java
+++ app/controllers/WayParticipation.java
@@ -7,5 +7,24 @@ import play.mvc.Controller;
  * @author pierregaste
  */
 public class WayParticipation extends Controller {
+
+    public static void add(){
+        render();
+    }
     
+    public static void edit(){
+        render();
+    }
+    
+    public static void cancel(){
+        render();
+    }
+    
+    public static void accept(){
+        render();
+    }
+
+    public static void refuse(){
+        render();
+    }
 }
