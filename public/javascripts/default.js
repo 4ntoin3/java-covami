d53d31dc7fb1bd49c8fc26cff93d03944b665ce6
Pierre Gasté <pierregaste@gmail.com>
2011-05-09 19:00:18 +0200

    Implémentation du plugin Timepicker pour les heures de départ des trajets

--- public/javascripts/default.js
+++ public/javascripts/default.js
@@ -11,8 +11,41 @@ $(document).ready(function(){
         window.location.replace("/"+(document.location.href).split("/")[3]);
     });
     
+    /**
+     * Add button
+     * 
+     * @element addButton class
+     * @event click
+     * @action Return to controller index
+     */
     $('.addButton').bind('click', function(){
-//        window.location.replace("/car/add");
         window.location.replace("/"+(document.location.href).split("/")[3]+"/add");
+    });
+    
+    /**
+     * Timepicker Field
+     * 
+     * @element timePickerField class
+     * @event Init timepicker plugin
+     * @action Enable field for timepicker
+     */
+    $('.timePickerField').timepicker({
+        timeOnlyTitle: 'Heure de départ',
+	timeText: 'Départ a',
+	hourText: 'Heure',
+	minuteText: 'Minute',
+	currentText: 'Heure actuelle',
+	closeText: 'Valider'
+    });
+    
+    /**
+     * Datepicker Field
+     * 
+     * @element datePickerField class
+     * @event Init datepicker plugin
+     * @action Enable field to datepicker
+     */
+    $('.datePickerField').datepicker({
+        minDate: 0
     });
 });
