9d13b0b43d10c2f11c85eef2b920ee8adcd1054f
Antoine <Antoine@192.168.1.10>
2011-05-10 19:30:15 +0200

    [New] classe représentant les routes

---
+++ app/models/Road.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import play.db.jpa.Model;
+
+/**
+ *
+ * @author Antoine
+ */
+@Entity
+public class Road extends Model{
+    
+    public String name;
+    
+    @ManyToOne
+    public City firstCity;
+    
+    @ManyToOne
+    public City secondCity;
+    
+    public Double distance;
+
+    public Road(String name, City firstCity, City secondCity) {
+        this.name = name;
+        this.firstCity = firstCity;
+        this.secondCity = secondCity;
+        this.distance = Math.sqrt(Math.pow((secondCity.latitude - firstCity.latitude), 2) + Math.pow((secondCity.longitude - firstCity.longitude), 2));
+    }       
+}
