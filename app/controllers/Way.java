dfff7f97d2a41e68763e6816da29773abedfbb05
Antoine <Antoine@Antoine-PC.home>
2011-05-15 16:36:41 +0200

    [Add] methode retournant le cout d'un trajet

--- app/controllers/Way.java
+++ app/controllers/Way.java
@@ -36,7 +36,7 @@ public class Way extends Controller {
         render(cities, cars);
     }
 
-    public static void addWay(@Required Long startCityId, @Required Long finishCityId, @Required Long carId, @Required Integer placeAvailable, @Required @As("dd/MM/yyyy") Date dateStart, @Required String hourStart) {
+    public static void addWay(@Required Long startCityId, @Required Long finishCityId, @Required Long carId, @Required Double cost, @Required Integer placeAvailable, @Required @As("dd/MM/yyyy") Date dateStart, @Required String hourStart) {
 
         validation.match(hourStart, "\\d\\d:\\d\\d").message("heure non valide");
         if (validation.hasErrors()) {
@@ -48,24 +48,28 @@ public class Way extends Controller {
         City startCity = City.findById(startCityId);
         City finishCity = City.findById(finishCityId);
         models.User driver = User.connected();
-        Double distance = new Double(0);
         dateStart.setHours(Integer.parseInt(hourStart.split(":")[0]));
         dateStart.setMinutes(Integer.parseInt(hourStart.split(":")[1]));
         models.Car car = models.Car.findById(carId);
         
-        models.Way way = new models.Way(startCity, finishCity, driver, distance, dateStart, car, placeAvailable);                
+        models.Way way = new models.Way(startCity, finishCity, driver, dateStart, car, placeAvailable, cost);                
         try {
             way.calculateWay();
         } catch (Exception ex) {
-//            validation.addError("user.email", "email used");
             params.put("fail", "error during the calcul");
-//            String fail = "error during the calcul";
             params.flash(); // add http parameters to the flash scope
             validation.keep(); // keep the errors for the next request
             add();
         }
         way.save();
         redirect("/way/list");
+    }
+    
+    public static Double calculCost(Long startCityId, Long finishCityId) throws Exception{
+        models.City startCity = models.City.findById(startCityId);
+        models.City finishCity = models.City.findById(finishCityId);
+        
+        return new models.Way(startCity, finishCity).cost();
     }
 
     public static void edit(Long id) {
@@ -75,7 +79,39 @@ public class Way extends Controller {
         
         render(way, cities, cars);
     }
+    
+    public static void editWay(Long id, @Required Long startCityId, @Required Long finishCityId, @Required Double cost, @Required Long carId, @Required Integer placeAvailable, @Required @As("dd/MM/yyyy") Date dateStart, @Required String hourStart) {
 
+        validation.match(hourStart, "\\d\\d:\\d\\d").message("heure non valide");
+        if (validation.hasErrors()) {
+            params.flash(); // add http parameters to the flash scope
+            validation.keep(); // keep the errors for the next request
+            add();
+        }
+        
+        City startCity = City.findById(startCityId);
+        City finishCity = City.findById(finishCityId);
+        dateStart.setHours(Integer.parseInt(hourStart.split(":")[0]));
+        dateStart.setMinutes(Integer.parseInt(hourStart.split(":")[1]));
+        models.Car car = models.Car.findById(carId);
+        
+        models.Way way = models.Way.findById(id);
+        way.startCity = startCity;
+        way.finishCity = finishCity;
+        way.dateHourStart = dateStart;
+        way.car = car;
+        
+        try {
+            way.calculateWay();
+        } catch (Exception ex) {
+            params.put("fail", "error during the calcul");
+            params.flash(); // add http parameters to the flash scope
+            validation.keep(); // keep the errors for the next request
+            add();
+        }
+        way.save();
+        redirect("/way/list");
+    }
     public static void details(Long id) {
         models.Way way = models.Way.findById(id);
         render(way);
