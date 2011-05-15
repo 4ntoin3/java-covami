5fc6d8a6692559be2755ab0e3d533b2c6c07ea37
Antoine <Antoine@192.168.1.10>
2011-05-15 22:54:45 +0200

    [Update] ajout d'une participation

--- app/controllers/WayParticipation.java
+++ app/controllers/WayParticipation.java
@@ -11,22 +11,30 @@ import play.mvc.With;
 public class WayParticipation extends Controller {
 
     public static void add(Long id){
+        models.Way way = models.Way.findById(id);
+        
+        new models.WayParticipation(way, User.connected(), 0).save();
+        
         redirect("/way/search");
     }
     
-    public static void edit(){
+    public static void cancel(Long id){
+        models.WayParticipation participation = models.WayParticipation.findById(id);
+        participation.status = 1;
+        
         redirect("/way/search");
     }
     
-    public static void cancel(){
-        redirect("/way/search");
-    }
-    
-    public static void accept(){
+    public static void accept(Long id){
+        models.WayParticipation participation = models.WayParticipation.findById(id);
+        participation.status = 2;
         redirect("/way/search");
     }
 
-    public static void refuse(){
+    public static void refuse(Long id){
+        models.WayParticipation participation = models.WayParticipation.findById(id);
+
+        
         redirect("/way/search");
     }
 }
