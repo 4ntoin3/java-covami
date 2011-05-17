a3f9151a0e53bc6aee32a930fff7c8964472e4d8
Pierre Gasté <pierregaste@gmail.com>
2011-05-17 09:10:10 +0200

    [Update] Ajout de commentaires, correction de l'affichage des kilometres dans les vues

--- app/controllers/Car.java
+++ app/controllers/Car.java
@@ -1,123 +1,135 @@
 package controllers;
 
 import java.util.List;
-import javax.persistence.Query;
 import play.data.validation.*;
-import play.db.jpa.JPA;
 import play.mvc.*;
 
-/**
- *
- * @author pierregaste
- */
 @With(Secure.class)
 public class Car extends Controller {
 
     /**
-     * Action par défaut
+     * [GET] Action par défaut
+     * 
      */
-    public static void index()
-    {
+    public static void index() {
         redirect("/car/list");
     }
-    
+
     /**
-     * Liste des voitures du compte connecté
+     * [GET] Liste les voitures de l'user connecté
      * 
-     * @view app/view/car/list.html
      */
-    public static void list()
-    {
-        List<Car> cars =  models.Car.find("owner = ? and deleted = 0", User.connected()).fetch();
+    public static void list() {
+        List<Car> cars = models.Car.find("owner = ? and deleted = 0", User.connected()).fetch();
+
         render(cars);
     }
-    
+
     /**
-     * Ajout une voiture pour le compte connecté
+     * [GET] Formulaire d'ajout de voiture
+     * 
      */
-    public static void add()
-    {        
+    public static void add() {
         render();
     }
-    
-    public static void addCar(@Valid models.Car car){
-        
+
+    /**
+     * [POST] Soumission d'une nouvelle voiture
+     * 
+     * @param car 
+     */
+    public static void addCar(@Valid models.Car car) {
         if (validation.hasErrors()) {
-            params.flash(); // add http parameters to the flash scope
-            validation.keep(); // keep the errors for the next request
+            params.flash();
+            validation.keep();
             add();
         }
-        
+
         new models.Car(car.name, car.nbPlace, car.cost, User.connected()).save();
 
         redirect("/car/list");
     }
-    
+
     /**
-     * Edit une voiture pour le compte connecté
+     * [GET] Formulaire d'édition de voiture
+     * 
+     * @param id 
      */
-    public static void edit(Long id)
-    {
-        checkIfTheCarBelongTheUserConnected(id);
-        models.Car car = models.Car.findById(id);              
+    public static void edit(Long id) {
+        _checkIfTheCarBelongTheUserConnected(id);
+        models.Car car = models.Car.findById(id);
+
         render(car);
     }
 
-    private static void checkIfTheCarBelongTheUserConnected(Long id) {
-        if(!models.Car.find("byOwner", User.connected()).fetch().contains(models.Car.findById(id))){
-            redirect("/car/list");
-        }
-    }
-    
-    public static void editCar(Long id, @Valid models.Car car){
-        checkIfTheCarBelongTheUserConnected(id);
+    /**
+     * [POST] Soumission d'édition d'une voiture
+     * 
+     * @param id
+     * @param car 
+     */
+    public static void editCar(Long id, @Valid models.Car car) {
+        _checkIfTheCarBelongTheUserConnected(id);
         models.Car carEdited = models.Car.findById(id);
-        
+
         if (validation.hasErrors()) {
-            params.flash(); // add http parameters to the flash scope
-            validation.keep(); // keep the errors for the next request
+            params.flash();
+            validation.keep();
             edit(carEdited.id);
         }
-        
+
         carEdited.name = car.name;
         carEdited.nbPlace = car.nbPlace;
         carEdited.cost = car.cost;
         carEdited.save();
-        
+
         redirect("/car/list");
     }
-    
+
     /**
-     * Supprime une voiture pour le compte connecté
+     * [GET] Soumission de suppression d'une voiture
+     * 
+     * @param id 
      */
-    public static void delete(Long id)
-    {
-        checkIfTheCarBelongTheUserConnected(id);
+    public static void delete(Long id) {
+        _checkIfTheCarBelongTheUserConnected(id);
         models.Car car = models.Car.findById(id);
         car.deleted = 1;
         car.save();
-        
+
         List<models.Way> ways = models.Way.find("byCar", car).fetch();
-        
+
         for (models.Way way : ways) {
             Way.cancel(way.id);
         }
-        
+
         redirect("/car/list");
     }
-    
+
     /**
-     * Affiche les informations d'une voiture
+     * [GET] Page de détails d'une voiture
+     * 
+     * @param id 
      */
-    public static void details(Long id)
-    {
-        checkIfTheCarBelongTheUserConnected(id);
+    public static void details(Long id) {
+        _checkIfTheCarBelongTheUserConnected(id);
         models.Car car = models.Car.findById(id);
         List<models.Way> ways = models.Way.find("byCar", car).fetch();
-        Query q = JPA.em().createQuery ("SELECT SUM(w.distance) FROM Way w WHERE car.id ="+car.id);
-        Number totalDistance = (Number) q.getSingleResult();
-        System.out.println("distance :"+totalDistance);
+        //Query q = JPA.em().createQuery("SELECT SUM(w.distance) FROM Way w WHERE car.id =" + car.id);
+        //Number totalDistance = (Number) q.getSingleResult();
+        //System.out.println("distance :" + totalDistance);
+        
         render(car, ways);
     }
-    
+
+    /**
+     * Vérifie qu'une voiture appartient à l'utilisateur connecté
+     * 
+     * @param id 
+     */
+    private static void _checkIfTheCarBelongTheUserConnected(Long id) {
+        if (!models.Car.find("byOwner", User.connected()).fetch().contains(models.Car.findById(id))) {
+            redirect("/car/list");
+        }
+    }
 }
