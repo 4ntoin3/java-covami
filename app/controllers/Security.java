3731fb49d2041cb906403ea3386429273203224e
Pierre Gasté <pierregaste@gmail.com>
2011-05-17 09:21:57 +0200

    [Update] Ajout de commentaires

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -1,44 +1,31 @@
 package controllers;
 
-/**
- * Classe de gestion de l'authentification de l'application
- * 
- * @version 1.0
- * @author Antoine
- */
 public class Security extends Secure.Security {
 
     /**
-     * Retourne si la tentative d'authentification à réussi ou non
+     * Méthode d'authentification
      * 
      * @param username
      * @param password
-     * @return boolean
+     * @return 
      */
     static boolean authentify(String username, String password) {
         return models.User.connect(username, password) != null;
     }
-
+    
     /**
-     * Méthode appellée lors de la déconnexion de l'utilisateur
+     * Hook sur la déconnexion
+     * 
      */
     static void onDisconnected() {
         User.index();
     }
     
     /**
-     * Méthode appellée lors de la connexion de l'utilisateur
+     * Hook sur la connexion
+     * 
      */
     static void onAuthenticated() {
          User.dashboard();
     }
-    
-    /*
-    static boolean check(String profile) {
-        if ("admin".equals(profile)) {
-            return models.User.find("byEmail", connected()).<models.User>first().isAdmin;
-        }
-        return false;  
-    }
-     * */
 }
